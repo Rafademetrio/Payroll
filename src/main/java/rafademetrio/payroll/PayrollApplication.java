418c6034a8fb0b2bbc12b3941e29a7b7d6e40bf2
Rafael Lucena <104261750+Rafademetrio@users.noreply.github.com>
2025-03-24 16:33:14 -0300

    Update PayrollApplication.java

--- src/main/java/rafademetrio/payroll/PayrollApplication.java
+++ src/main/java/rafademetrio/payroll/PayrollApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PayrollApplication {
-
 	public static void main(String[] args) {
 		System.out.println("PGHOST: " + System.getenv("PGHOST"));
 		System.out.println("PGPORT: " + System.getenv("PGPORT"));
@@ -13,5 +12,4 @@ public class PayrollApplication {
 		System.out.println("PGUSER: " + System.getenv("PGUSER"));
 		SpringApplication.run(PayrollApplication.class, args);
 	}
-
 }
