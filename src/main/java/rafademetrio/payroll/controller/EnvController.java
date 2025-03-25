642c7157241784a65ba4c6af8c8e0597b6c74c05
Rafael Lucena <rafademetrio@gmail.com>
2025-03-25 09:04:09 -0300

    7th attemp: postgres hardcoded - correct

--- src/main/java/rafademetrio/payroll/controller/EnvController.java
+++ src/main/java/rafademetrio/payroll/controller/EnvController.java
@@ -1,2 +1,15 @@
-package rafademetrio.payroll.controller;public class EnvController {
+package rafademetrio.payroll.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    @GetMapping("/env")
+    public Map<String,String> getEnv(){
+        return System.getenv();
+    }
 }
